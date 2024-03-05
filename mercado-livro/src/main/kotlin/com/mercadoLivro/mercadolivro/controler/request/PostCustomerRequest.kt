2fcdfb39de7e69bf695fa8a68f21ea3f6626db65
tcriga <thay.c.gayv@outlook.com>
2024-03-05 15:00:04 -0400

    create communication with mysql

--- mercado-livro/src/main/kotlin/com/mercadoLivro/mercadolivro/controler/request/PostCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadoLivro/mercadolivro/controler/request/PostCustomerRequest.kt
@@ -1,5 +1,7 @@
 package com.mercadoLivro.mercadolivro.controler.request
 
+import com.mercadoLivro.mercadolivro.model.CustomerModel
+
 data class PostCustomerRequest(
     var name: String,
     var email: String
